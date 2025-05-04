097b2bd3c1b7d147ebcc50be77cb4f511859e9ad
ZidiEya <91125384+ZidiEya@users.noreply.github.com>
2025-05-04 22:02:08 +0100

    update function
    Each row in Pascal’s triangle:
    
    Starts and ends with 1.
    
    Each interior number is the sum of the two numbers directly above it.

--- 0-pascal_triangle.py
+++ 0-pascal_triangle.py
@@ -1,2 +1,24 @@
-GitHub repository: holbertonschool-interview
-Directory: pascal_triangle
+#!/usr/bin/python3
+"""
+Function that returns Pascal's Triangle of n
+"""
+
+
+def pascal_triangle(n):
+    """
+    Returns a list of lists of integers representing the Pascal’s triangle of n
+    """
+    if n <= 0:
+        return []
+
+    triangle = [[1]]  # First row
+
+    for i in range(1, n):
+        row = [1]  # Start each row with 1
+        for j in range(1, i):
+            # Sum of two elements directly above
+            row.append(triangle[i - 1][j - 1] + triangle[i - 1][j])
+        row.append(1)  # End each row with 1
+        triangle.append(row)
+
+    return triangle
